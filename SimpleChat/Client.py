a69e09b0958efd2374d725c0c8f20828172a812d
Abdullah-khallaf <abdullah.khallaf.work@gmail.com>
2024-05-14 01:25:05 +0300

    Establishing server and client

---
+++ SimpleChat/Client.py
@@ -0,0 +1,18 @@
+from socket import *
+# Create a socket object
+S = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Get the server hostname and port
+host = "127.0.0.1"
+port = 8000 # Same port as the server
+
+# Connect to the server
+S.connect((host, port))
+
+# Send data to server
+message = "Hello, From Abdullah"
+while True:
+    S.send(input("Client: ").encode())
+    x=S.recv(2048)
+    print("server: " + x.decode())
+S.close()
---
+++ SimpleChat/Server.py
@@ -0,0 +1,24 @@
+from socket import *
+
+try:
+    print("Server started")
+    S = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    Host = "127.0.0.1"
+    Port = 8000
+    S.bind((Host, Port))
+    S.listen(5)
+    
+    conn, addr = S.accept()
+    print("connection from ", addr[1])
+    while True:
+        x = conn.recv(2048)
+        print("client: ", x.decode())
+        conn.send(input("server: ").encode())
+    S.close()
+except error as e:
+    print(e)
+except KeyboardInterrupt:
+    print("OK")
+    
+
